80c05c9e4471c6dd2e2d580a6d857771acd4022f
Israel Rodrigo <israel.code.rodrigo.11@gmail.com>
2023-07-05 10:39:31 +0200

    Terminada funcionalidad Cronometro

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const btnReloj=document.querySelector(".btnReloj"),
-      btnAlarma=document.querySelector(".btnTempo"),
-      btnTempo=document.querySelector(".btnAlarma"),
+      btnTempo=document.querySelector(".btnTempo"),
+      btnAlarma=document.querySelector(".btnAlarma"),
       num=document.querySelectorAll(".num"),
       puntos=document.querySelectorAll(".punto"),
       btnSubir=document.querySelector(".btnSubir"),
-      btnBajar=document.querySelector(".btnBajar");
+      btnBajar=document.querySelector(".btnBajar"),
+      btnParo=document.querySelector(".btnParo");
+
+const pos1=document.querySelector(".pos1"),
+      pos2=document.querySelector(".pos2"),
+      pos3=document.querySelector(".pos3"),
+      pos4=document.querySelector(".pos4");
 
 let segundos,
     temporizador;
+
     
 
 
 
 btnReloj.addEventListener("click",e=>{
+    borrarReloj();
     btnReloj.classList.add("btnImpTemAlar");
     btnAlarma.classList.remove("btnImpTemAlar");
     btnTempo.classList.remove("btnImpTemAlar");
-    
     temporizador=setInterval(e=>{
         let tiempo=reloj();
         if(segundos==0){
@@ -39,8 +46,29 @@ btnAlarma.addEventListener("click",e=>{
     btnAlarma.classList.add("btnImpTemAlar");
     btnTempo.classList.remove("btnImpTemAlar");
     borrarReloj();
+    console.log("Estas en el temporizador");
 
-
+    pos1.addEventListener("click",e=>{
+        console.log(e);
+        let position=0;  
+        seleccionNumero(position);      
+    });
+    pos2.addEventListener("click",e=>{
+        console.log(e);
+        let position=21;
+        seleccionNumero(position);
+    })
+    pos3.addEventListener("click",e=>{
+        console.log(e);
+        let position=2;
+        seleccionNumero(position);
+    });
+    pos4.addEventListener("click",e=>{
+        console.log(e);
+        let position=3;
+        seleccionNumero(position);
+    });
+    
 });
 
 btnTempo.addEventListener("click",e=>{
@@ -48,14 +76,16 @@ btnTempo.addEventListener("click",e=>{
     btnAlarma.classList.remove("btnImpTemAlar");
     btnTempo.classList.add("btnImpTemAlar");
     borrarReloj();
+    let etapa=0;
+    inicialNumero();
+    btnParo.addEventListener("click",e=>{
+        if(etapa==3)etapa=0;
+        etapa++;
+        tempo(etapa);
+    })
 
-
+    
 });
-
-
-
-
-
 function parpadeo(seg){
     if(seg%2==0){
         for(let i=0;i<2;i++){
@@ -107,4 +137,72 @@ function pintarNumero(arr,i){
         num[i].children[e].style.opacity="1";
     })
 
+}
+function borrarNumero(dig){
+    for(let o=0;o<7;o++){
+        num[dig].children[o].removeAttribute("style");
+    }
+    
+}
+function inicialNumero(){
+    let arr=[0,0,0,0];
+    arr.forEach((e,i)=>{
+        for(let o=0;o<7;o++){
+            num[i].children[o].removeAttribute("style");
+        }
+        let dig=obtenerNumero(e);
+        pintarNumero(dig,i);
+    })
+    for(let i=0;i<2;i++){
+        puntos[i].style.opacity="1";
+    }
+}
+function tempo(etapa){
+    let arr=[0,0,0,0];
+    if(etapa==1){
+        //iniciar temporizador
+        temporizador=setInterval(e=>{
+            borrarNumero(3);
+            arr[3]++;
+            if(arr[3]>9){
+              borrarNumero(2);
+              arr[3]=0;
+              arr[2]++;
+              if(arr[2]>9){
+                borrarNumero(1);
+                    arr[2]=0;
+                    arr[1]++;
+                    if(arr[1]>9){
+                        borrarNumero(0);
+                        arr[1]=0;
+                        arr[0]++;
+                        if(arr[0]==5){
+                            clearInterval(temporizador);
+                        }
+                    }
+                }
+            }
+            arr.forEach((e,i,arr)=>{
+                let numPint=obtenerNumero(e);
+                pintarNumero(numPint,i);
+            })
+            console.log(arr[0]%2==0)
+            if(arr[3]%2==0){
+                for(let i=0;i<2;i++){
+                    puntos[i].style.opacity="1";
+                }
+            }else{
+                for(let i=0;i<2;i++){
+                    puntos[i].removeAttribute("style");
+                }
+            }
+        },1000);
+    }else if(etapa==2){
+        //parar temporizador
+        clearInterval(temporizador);
+    }else if(etapa==3){
+        //reiniciar temporizador
+        inicialNumero()
+    }
+    
 }
